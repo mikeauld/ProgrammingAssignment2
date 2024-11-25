4d0cf66632ce0441bc4924b26cc1f8e40906df2c
Michael Auld <113619177+mikeauld@users.noreply.github.com>
2024-11-25 11:44:10 +0000

    Add files via upload
    Updated with my solution to programming Assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,38 @@
-## Put comments here that give an overall description of what your
+## This script creates two functions that take advantage of the 
+## scoping rules of R, for the Week 2 assignment
 ## functions do
 
-## Write a short comment describing this function
+## makeCacheMatrix creates a list of functions 
+## associated with a matrix, to save on computing power
+## and holds the matrix in cache memory for using later
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    m <- NULL
+    set <- function(y) {
+        x <<- y
+        m <<- NULL
+    }
+    get <- function() x
+    setinverse <- function(solve) m <<- solve
+    getinverse <- function() m
+    list(set = set, get = get,
+                       setinverse = setinverse,
+                       getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## cacheSolve uses the cached matrix created earlier and 
+## performs the inverse function on the matrix
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+    m <- x$getinverse()
+    if(!is.null(m)) {
+        message("getting cached data")
+        return(m)
+    }
+    data <- x$get()
+    m <- solve(data, ...)
+    x$setinverse(m)
+    m
 }
